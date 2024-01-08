98f6f291f048e64c07659be0b26df739c87d91aa
Progi1984 <franck.lefevre@prestashop.com>
2024-01-08 15:32:06 +0100

    Basic UI Testing Library

---
+++ src/interfaces/BO/dashboard/index.ts
@@ -0,0 +1,3 @@
+export interface DashboardPageInterface {
+    readonly pageTitle: string;
+}
---
+++ src/pages/BO/login/index.ts
@@ -0,0 +1,11 @@
+import semver from "semver";
+
+let file: string;
+if (semver.gte(process.env.PS_VERSION as string, '8.0.0')) {
+    file = "@versions/8.0.0/pages/BO/login";
+} else {
+    file = "@versions/8.0.0/pages/BO/login"
+}
+
+const Login = await import(file);
+export default Login;
