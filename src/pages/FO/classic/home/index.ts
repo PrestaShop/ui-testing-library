2101800929935a1d1e7f87cca8bd18a35e8c482f
Progi1984 <franck.lefevre@prestashop.com>
2024-03-20 15:53:30 +0100

    1.7.7 : FO - Classic  - Home : Fixed the product article selector

--- src/pages/FO/classic/home/index.ts
+++ src/pages/FO/classic/home/index.ts
@@ -6,10 +6,11 @@ const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-var-requires */
 function requirePage(): FoHomePageInterface {
-  if (semver.gte(psVersion, '8.0.0')) {
+  // >= 1.7.7.9
+  if (semver.gte(psVersion, '7.7.0')) {
     return require('@versions/develop/pages/FO/classic/home').homePage;
   }
-  return require('@versions/1.7.8/pages/FO/classic/home');
+  return require('@versions/1.7.6/pages/FO/classic/home');
 }
 /* eslint-enable global-require, @typescript-eslint/no-var-requires */
 
