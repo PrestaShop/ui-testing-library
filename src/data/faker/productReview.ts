57885609ed93b060ae97abe2fdfb1d78a67b53e7
Progi1984 <franck.lefevre@prestashop.com>
2024-03-27 14:48:04 +0100

    Added @data/demo/products

--- src/data/faker/productReview.ts
+++ src/data/faker/productReview.ts
@@ -14,17 +14,17 @@ export default class FakerProductReview {
   public reviewRating: number;
 
   /**
-     * Constructor for class ProductReview
-     * @param productReviewToCreate {ProductReviewCreator} Could be used to add a review on a product
-     */
+   * Constructor for class ProductReview
+   * @param productReviewToCreate {ProductReviewCreator} Could be used to add a review on a product
+   */
   constructor(productReviewToCreate: ProductReviewCreator = {}) {
     /** @type {string} Title of the review */
     this.reviewTitle = productReviewToCreate.reviewTitle
-            || faker.lorem.sentence(faker.number.int({min: 3, max: 7}));
+      || faker.lorem.sentence(faker.number.int({min: 3, max: 7}));
 
     /** @type {string} Content of the review */
     this.reviewContent = productReviewToCreate.reviewContent
-            || faker.lorem.sentences(faker.number.int({min: 3, max: 10}));
+      || faker.lorem.sentences(faker.number.int({min: 3, max: 10}));
 
     /** @type {number} Rating of the review */
     this.reviewRating = productReviewToCreate.reviewRating || faker.number.int({min: 1, max: 5});
