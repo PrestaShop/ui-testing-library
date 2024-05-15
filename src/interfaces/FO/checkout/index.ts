bee7fb78cfad480fd85a69e28668ad9a34f14bf5
Progi1984 <franck.lefevre@prestashop.com>
2024-05-15 16:10:41 +0200

    Support for TEST-971

--- src/interfaces/FO/checkout/index.ts
+++ src/interfaces/FO/checkout/index.ts
@@ -3,8 +3,13 @@ import type {Page} from '@playwright/test';
 
 export interface FoCheckoutPageInterface extends FOBasePagePageInterface {
     personalInformationStepForm: string;
+    readonly noCarriersMessage: string;
 
     choosePaymentAndOrder(page: Page, paymentModuleName: string): Promise<void>;
+    clickOnSignIn(page: Page): Promise<void>;
+    customerLogin(page: Page, customer: any): Promise<boolean>;
+    getAllCarriersNames(page: Page): Promise<(string | null)[]>;
+    getCarrierErrorMessage(page: Page): Promise<string|null>;
     goToDeliveryStep(page: Page): Promise<boolean>;
     goToPaymentStep(page: Page): Promise<boolean>;
     isCheckoutPage(page: Page): Promise<boolean>;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,7 @@
 import type {BrowserContext, Page} from '@playwright/test';
 
 export interface CommonPageInterface {
+    changePage(browserContext: BrowserContext, tabId?: number): Promise<Page>;
     closePage(browserContext: BrowserContext, page: Page, tabId?: number): Promise<Page>;
     getPageTitle(page: Page): Promise<string>;
     goTo(page: Page, url: string): Promise<void>;
