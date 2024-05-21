03cfd9dad56c48514931e9de9f206b342dd41a7a
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-05-21 17:33:34 +0200

    Fix review

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ export {default as boLoginPage} from '@pages/BO/login';
 export {default as boDashboardPage} from '@pages/BO/dashboard';
 export {default as boOrdersPage} from '@pages/BO/orders';
 export {default as boOrdersBasePage} from '@pages/BO/orders/view/viewOrderBasePage';
-export {default as boViewOrderProductsBlockPage} from '@pages/BO/orders/view/productsBlock';
+export {default as boOrdersViewProductsBlockPage} from '@pages/BO/orders/view/productsBlock';
 export {default as boModuleManagerPage} from '@pages/BO/modules/moduleManager';
 export {default as boModuleManagerUninstalledModulesPage} from '@pages/BO/modules/moduleManager/uninstalledModules';
 export {default as boDesignPositionsPage} from '@pages/BO/design/positions/index';
