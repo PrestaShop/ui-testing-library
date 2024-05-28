864d7dbfcd124d5194843c62f05b3935b9e3646f
Progi1984 <franck.lefevre@prestashop.com>
2024-05-28 17:32:05 +0200

    Fixed import

--- src/pages/BO/catalog/products/index.ts
+++ src/pages/BO/catalog/products/index.ts
@@ -1,15 +1,8 @@
 import type {BOProductsPageInterface} from '@interfaces/BO/catalog/products';
-import testContext from '@utils/test';
-import semver from 'semver';
-
-const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require */
 function requirePage(): BOProductsPageInterface {
-  if (semver.gte(psVersion, '0.0.0')) {
-    return require('@versions/develop/pages/BO/catalog/products');
-  }
-  return require('@versions/develop/pages/BO/catalog/products');
+  return require('@versions/develop/pages/BO/catalog/products').productsPage;
 }
 /* eslint-enable global-require */
 
