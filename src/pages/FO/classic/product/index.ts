2a116d6f82d9e5032529cca36bc7d78fc8a3c808
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-05-28 13:50:30 +0200

    Fix product page

--- src/pages/FO/classic/product/index.ts
+++ src/pages/FO/classic/product/index.ts
@@ -7,9 +7,9 @@ const psVersion = testContext.getPSVersion();
 /* eslint-disable global-require, @typescript-eslint/no-var-requires */
 function requirePage(): FoProductPageInterface {
   if (semver.gte(psVersion, '0.0.0')) {
-    return require('@versions/develop/pages/FO/classic/product').cartPage;
+    return require('@versions/develop/pages/FO/classic/product').productPage;
   }
-  return require('@versions/develop/pages/FO/classic/product').cartPage;
+  return require('@versions/develop/pages/FO/classic/product').productPage;
 }
 /* eslint-enable global-require, @typescript-eslint/no-var-requires */
 
