d9cf6ca51d0bd3c673e3f793777d85b7f173fb50
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-05-29 22:40:28 +0200

    Fix lint error

--- src/pages/BO/catalog/products/index.ts
+++ src/pages/BO/catalog/products/index.ts
@@ -1,9 +1,17 @@
 import type {BOProductsPageInterface} from '@interfaces/BO/catalog/products';
+import semver from 'semver';
+import testContext from '@utils/test';
+
+const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require */
 function requirePage(): BOProductsPageInterface {
-  return require('@versions/develop/pages/BO/catalog/products').productsPage;
+    if (semver.gte(psVersion, '0.0.0')) {
+        return require('@versions/develop/pages/BO/catalog/products').productsPage;
+    }
+    return require('@versions/develop/pages/BO/catalog/products');
 }
+
 /* eslint-enable global-require */
 
 export default requirePage();
