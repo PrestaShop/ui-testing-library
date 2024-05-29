9ca843a5b33745ca391faef26f558b6dfbb02734
Progi1984 <franck.lefevre@prestashop.com>
2024-05-29 11:10:00 +0200

    Added waitForSelector to CommonPageInterface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,4 @@
+import type {PageWaitForSelectorOptionsState} from '@data/types/playwright';
 import type {BrowserContext, Frame, Page} from '@playwright/test';
 
 export interface CommonPageInterface {
@@ -8,5 +9,6 @@ export interface CommonPageInterface {
     goToFo(page: Page): Promise<void>;
     reloadPage(page: Page): Promise<void>;
     resize(page: Page, mobileSize: boolean): Promise<void>;
+    waitForSelector(page: Page | Frame, selector: string, state: PageWaitForSelectorOptionsState, timeout?: number): Promise<void>
     waitForSelectorAndClick(page: Frame | Page, selector: string, timeout?: number): Promise<void>
 }
