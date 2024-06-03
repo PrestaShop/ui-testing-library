38a4a36ffe2f448398b13844ecb66a14ab94e7f5
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-06-03 16:07:44 +0200

    Fix lint errors-2

--- src/pages/BO/catalog/products/create/index.ts
+++ src/pages/BO/catalog/products/create/index.ts
@@ -11,6 +11,6 @@ function requirePage(): BOProductsCreatePageInterface {
   }
   return require('@versions/develop/pages/BO/catalog/products/create').createProduct;
 }
-/* eslint-disable global-require, @typescript-eslint/no-var-requires */
+/* eslint-enable global-require, @typescript-eslint/no-var-requires */
 
 export default requirePage();
