f5e30b46e0a42e8a5e12cc24613cb298e7a3e966
Progi1984 <franck.lefevre@prestashop.com>
2024-06-10 15:27:07 +0200

    Migrate `@data/types/*` from Core

---
+++ src/data/types/localization.ts
@@ -0,0 +1,10 @@
+type ImportContent = {
+  importStates?: boolean,
+  importTaxes?: boolean,
+  importCurrencies?: boolean,
+  importLanguages?: boolean,
+  importUnits?: boolean,
+  updatePriceDisplayForGroups?: boolean,
+};
+
+export default ImportContent;
