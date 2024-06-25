9bae64d28030a85d93abfdb48e31dc237a6c84cf
Progi1984 <franck.lefevre@prestashop.com>
2024-06-25 17:10:25 +0200

    `@pages\FO\searchResults` : Fixed some missing methods

--- src/interfaces/FO/searchResults/index.ts
+++ src/interfaces/FO/searchResults/index.ts
@@ -4,9 +4,12 @@ import type {Page} from '@playwright/test';
 export interface FoSearchResultsPageInterface extends FOBasePagePageInterface {
   readonly pageTitle: string;
 
+  getAllProductsAttribute(page: Page, attribute: string): Promise<string[]>;
   getProductPrice(page: Page): Promise<string>;
   getSearchResultsNumber(page: Page): Promise<number>;
+  getSortByValue(page: Page): Promise<string>;
   goToProductPage(page: Page, id: number): Promise<void>;
   hasResults(page: Page): Promise<boolean>;
   quickViewProduct(page: Page, id: number): Promise<void>;
+  sortProductsList(page: Page, sortBy: string): Promise<void>;
 }
