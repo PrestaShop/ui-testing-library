9263c6df845edadb49ae0fffcb482d371e382c22
Progi1984 <franck.lefevre@prestashop.com>
2024-07-16 10:33:03 +0200

    `@pages/BO/shopParameters/search` : Added methods for Indexing block

--- src/interfaces/BO/shopParameters/search/index.ts
+++ src/interfaces/BO/shopParameters/search/index.ts
@@ -5,16 +5,20 @@ export interface BOSearchPageInterface extends BOBasePagePageInterface {
     readonly pageTitle: string;
     readonly settingsUpdateMessage: string;
     readonly successfulUpdateStatusMessage: string;
+    readonly updateSuccessMessage: string;
 
     bulkDeleteAliases(page: Page): Promise<string>;
     bulkSetStatus(page: Page, enable?: boolean): Promise<string>;
     deleteAlias(page: Page, row: number): Promise<string>;
+    clickAddMissingProductsToIndex(page: Page): Promise<string>;
     filterTable(page: Page, filterType: string, filterBy: string, value: string): Promise<void>;
     getAllRowsColumnContent(page: Page, columnName: string): Promise<string[]>;
     getBlacklistedWords(page: Page, idLang: number): Promise<string>;
     getMaximumApproximateWords(page: Page): Promise<number>;
     getMinimumWordLength(page: Page): Promise<number>;
     getNumberOfElementInGrid(page: Page): Promise<number>;
+    getNumIndexedProducts(page: Page): Promise<number>;
+    getNumTotalProducts(page: Page): Promise<number>;
     getStatus(page: Page, row: number): Promise<boolean>;
     getTextColumn(page: Page, row: number, columnName: string): Promise<string>;
     goToAddNewAliasPage(page: Page): Promise<void>;
@@ -27,6 +31,7 @@ export interface BOSearchPageInterface extends BOBasePagePageInterface {
     selectPaginationLimit(page: Page, number: number): Promise<string>;
     setBlacklistedWords(page: Page, idLang: number, words: string): Promise<string>;
     setFuzzySearch(page: Page, toEnable?: boolean): Promise<string>;
+    setIndexing(page: Page, toEnable?: boolean): Promise<string>;
     setMaximumApproximateWords(page: Page, maxWords: number): Promise<string>;
     setMinimumWordLength(page: Page, length: number): Promise<string>;
     setStatus(page: Page, row: number, valueWanted?: boolean): Promise<boolean>;
