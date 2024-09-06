8a287e8e3ed8f8efd4d6fd77bc95f690f611341e
Progi1984 <franck.lefevre@prestashop.com>
2024-09-06 14:13:06 +0200

    Migrate `@pages/BO/customers` from Core

---
+++ src/pages/BO/customers/index.ts
@@ -0,0 +1,9 @@
+import {type BOCustomersPageInterface} from '@interfaces/BO/customers';
+
+/* eslint-disable global-require, @typescript-eslint/no-var-requires */
+function requirePage(): BOCustomersPageInterface {
+  return require('@versions/develop/pages/BO/customers');
+}
+/* eslint-enable global-require, @typescript-eslint/no-var-requires */
+
+export default requirePage();
