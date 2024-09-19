a055f59aac2532999d189a801f71ea237a24ffac
Progi1984 <franck.lefevre@prestashop.com>
2024-09-19 17:13:22 +0200

    Migrate `@pages/BO/orders` from Core

--- src/interfaces/BO/orders/index.ts
+++ src/interfaces/BO/orders/index.ts
@@ -9,7 +9,7 @@ export interface BOOrdersPageInterface extends BOBasePagePageInterface {
     bulkOpenInNewTabs(page:Page, isAllOrders:boolean, row:number[]):Promise<Page>;
     bulkUpdateOrdersStatus(page:Page, status:string, isAllOrders:boolean, rows:number[]):Promise<string>;
     clickOnBulkActionsButton(page:Page):Promise<void>;
-    clickOnMoreLink(page: Page, row: number): Promise<void>;
+    clickOnMoreLink(page: Page, row?: number): Promise<void>;
     downloadDeliverySlip(page:Page, row:number):Promise<string|null>;
     downloadInvoice(page:Page, row:number):Promise<string|null>;
     exportDataToCsv(page:Page):Promise<string | null>;
@@ -20,7 +20,7 @@ export interface BOOrdersPageInterface extends BOBasePagePageInterface {
     getCustomerInvoiceAddressDetails(page: Page): Promise<string>;
     getNumberOfElementInGrid(page:Page):Promise<number>;
     getNumberOfOrdersInPage(page:Page):Promise<number>;
-    getOrderATIPrice(page:Page, row:number):Promise<number>;
+    getOrderATIPrice(page:Page, row?:number):Promise<number>;
     getOrderFromTable(page:Page, row:number):Promise<{
         id: number,
         reference: string,
@@ -31,19 +31,19 @@ export interface BOOrdersPageInterface extends BOBasePagePageInterface {
         payment: string,
         status: string,
     }>
-    getOrderIDNumber(page:Page, row:number):Promise<number>;
+    getOrderIDNumber(page:Page, row?:number):Promise<number>;
     getOrderInCsvFormat(page:Page, row:number):Promise<string>;
     getPaginationLabel(page: Page): Promise<string>;
     getProductDetailsFromTable(page: Page, row: number): Promise<string>;
     getProductsNumberFromTable(page: Page): Promise<number>;
     getShippingDetails(page: Page): Promise<string>;
-    getTextColumn(page: Page, columnName: string, row: number): Promise<string>;
+    getTextColumn(page: Page, columnName: string, row?: number): Promise<string>;
     goToCreateOrderPage(page:Page):Promise<void>;
     goToOrder(page:Page, row:number):Promise<void>;
     openOrderDetails(page: Page): Promise<void>;
     paginationNext(page: Page): Promise<string>;
     paginationPrevious(page: Page): Promise<string>;
-    previewOrder(page: Page, row: number): Promise<boolean>;
+    previewOrder(page: Page, row?: number): Promise<boolean>;
     resetAndGetNumberOfLines(page:Page):Promise<number>;
     resetFilter(page:Page):Promise<void>;
     selectAllOrders(page:Page):Promise<void>;
