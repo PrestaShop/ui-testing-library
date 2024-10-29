721cce2aeef3485e9bc44844fa192cc26dac55dd
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-10-29 16:26:52 +0100

    Add 9.0 version

--- src/pages/BO/catalog/suppliers/create.ts
+++ src/pages/BO/catalog/suppliers/create.ts
@@ -1,9 +1,17 @@
 import type {BOSuppliersCreatePageInterface} from '@interfaces/BO/catalog/suppliers/create';
+import testContext from '@utils/test';
+import semver from 'semver';
+
+const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-var-requires */
 function requirePage(): BOSuppliersCreatePageInterface {
-  return require('@versions/develop/pages/BO/catalog/suppliers/create');
+  if (semver.gte(psVersion, '9.0.0')) {
+    return require('@versions/9.0/pages/BO/catalog/suppliers/create').createSupplier;
+  }
+  return require('@versions/develop/pages/BO/catalog/suppliers/create').createSupplier;
 }
+
 /* eslint-enable global-require, @typescript-eslint/no-var-requires */
 
 export default requirePage();
--- src/versions/develop/pages/BO/catalog/suppliers/create.ts
+++ src/versions/develop/pages/BO/catalog/suppliers/create.ts
@@ -13,55 +13,55 @@ class BOSuppliersCreatePage extends BOBasePage implements BOSuppliersCreatePageI
 
   public readonly pageTitleEdit: string;
 
-  private readonly nameInput: string;
+  protected nameInput: string;
 
   private readonly descriptionDiv: string;
 
   private readonly descriptionLangNavItemLink: (lang: string) => string;
 
-  private readonly descriptionIFrame: (id: number) => string;
+  protected descriptionIFrame: (id: number) => string;
 
-  private readonly homePhoneInput: string;
+  protected homePhoneInput: string;
 
-  private readonly mobilePhoneInput: string;
+  protected mobilePhoneInput: string;
 
-  private readonly addressInput: string;
+  protected addressInput: string;
 
-  private readonly secondaryAddressInput: string;
+  protected secondaryAddressInput: string;
 
-  private readonly postalCodeInput: string;
+  protected postalCodeInput: string;
 
-  private readonly cityInput: string;
+  protected cityInput: string;
 
   private readonly countryInput: string;
 
-  private readonly selectCountryList: string;
+  protected selectCountryList: string;
 
-  private readonly searchCountryInput: string;
+  protected searchCountryInput: string;
 
-  private readonly countrySearchResult: string;
+  protected countrySearchResult: string;
 
   private readonly stateInput: string;
 
-  private readonly logoFileInput: string;
+  protected logoFileInput: string;
 
   private readonly metaTitleLangButton: string;
 
   private readonly metaTitleLangSpan: (lang: string) => string;
 
-  private readonly metaTitleInput: (id: number) => string;
+  protected metaTitleInput: (id: number) => string;
 
-  private readonly metaDescriptionTextarea: (id: number) => string;
+  protected metaDescriptionTextarea: (id: number) => string;
 
   private readonly metaKeywordsInput: (id: number) => string;
 
-  private readonly statusToggleInput: (toggle: number) => string;
+  protected statusToggleInput: (toggle: number) => string;
 
   private readonly taggableFieldDiv: (lang: string) => string;
 
   private readonly deleteKeywordLink: (lang: string) => string;
 
-  private readonly saveButton: string;
+  protected saveButton: string;
 
   /**
    * @constructs
@@ -214,4 +214,5 @@ class BOSuppliersCreatePage extends BOBasePage implements BOSuppliersCreatePageI
   }
 }
 
-module.exports = new BOSuppliersCreatePage();
+const createSupplier = new BOSuppliersCreatePage();
+export {createSupplier, BOSuppliersCreatePage as CreateSupplier};
