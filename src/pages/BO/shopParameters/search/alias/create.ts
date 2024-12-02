4cb894d8ed23981eac1bf68c84b85c92c02b3688
Progi1984 <franck.lefevre@prestashop.com>
2024-12-02 15:57:18 +0100

    `boSearchAliasPage` : Adapt the page for v9

--- src/pages/BO/shopParameters/search/alias/create.ts
+++ src/pages/BO/shopParameters/search/alias/create.ts
@@ -1,8 +1,15 @@
 import type {BOAliasCreatePageInterface} from '@interfaces/BO/shopParameters/search/alias/create';
+import testContext from '@utils/test';
+import semver from 'semver';
+
+const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BOAliasCreatePageInterface {
-  return require('@versions/develop/pages/BO/shopParameters/search/alias/create');
+  if (semver.lt(psVersion, '9.0.0')) {
+    return require('@versions/8.2/pages/BO/shopParameters/search/alias/create');
+  }
+  return require('@versions/develop/pages/BO/shopParameters/search/alias/create').boSearchAliasCreatePage;
 }
 /* eslint-enable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 
