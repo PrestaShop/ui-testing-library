4fce08144e82a586aacfdf7dd38d0cead6171e69
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-12-18 14:25:22 +0100

    Fix review

--- src/pages/BO/modules/autoupgrade/index.ts
+++ src/pages/BO/modules/autoupgrade/index.ts
@@ -1,14 +1,8 @@
 import type {ModuleAutoupgradeMainPageInterface} from '@interfaces/BO/modules/autoupgrade';
 import testContext from '@utils/test';
-import semver from 'semver';
-
-const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-require-imports */
 function requirePage(): ModuleAutoupgradeMainPageInterface {
-  if (semver.gte(psVersion, '0.0.0')) {
-    return require('@versions/develop/pages/BO/modules/autoupgrade');
-  }
   return require('@versions/develop/pages/BO/modules/autoupgrade');
 }
 
