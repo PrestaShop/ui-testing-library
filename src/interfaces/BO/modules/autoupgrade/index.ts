017f44e5752d9faed31be930afd266d3ea311f5c
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2025-01-08 17:41:49 +0100

    Add methods for new UI of Autoupgrade module

--- src/interfaces/BO/modules/autoupgrade/index.ts
+++ src/interfaces/BO/modules/autoupgrade/index.ts
@@ -4,6 +4,20 @@ import type {Page} from '@playwright/test';
 export interface ModuleAutoupgradeMainPageInterface extends ModuleConfigurationPageInterface {
   readonly pageTitle: string;
 
-  goToMaintenancePage(page:Page): Promise<Page>;
-  isRequirementsAlertDangerVisible(page:Page): Promise<boolean>;
+  readonly checkRequirementSuccessMessage: string;
+
+  readonly updateSuccessMessage: string;
+
+  cancelBackup(page:Page):Promise<boolean>;
+  checkRequirements(page: Page): Promise<string>;
+  checkUpdateSuccess(page:Page):Promise<string>;
+  chooseNewVersion(page:Page):Promise<boolean>;
+  clickOnLaunchBackup(page:Page):Promise<boolean>;
+  clickOnUpdateWithoutBackup(page:Page):Promise<void>;
+  getCurrentPSAndPHPVersion(page: Page): Promise<string>;
+  getNewPSVersion(page: Page): Promise<string>;
+  getStepTitle(page:Page):Promise<string>;
+  goToMaintenancePage(page: Page): Promise<Page>;
+  goToNextStep(page:Page):Promise<void>;
+  updateYourStore(page: Page): Promise<boolean>;
 }
