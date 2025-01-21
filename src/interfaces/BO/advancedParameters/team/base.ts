a6c822a4a7926b853668e70f3119fad77816731c
Progi1984 <franck.lefevre@prestashop.com>
2025-01-21 15:31:35 +0100

    `BOTeamBasePageInterface` : Added method `cancel`

--- src/interfaces/BO/advancedParameters/team/base.ts
+++ src/interfaces/BO/advancedParameters/team/base.ts
@@ -1,6 +1,9 @@
 import {BOBasePagePageInterface} from '@interfaces/BO';
+import {type Page} from '@playwright/test';
 
 export interface BOTeamBasePageInterface extends BOBasePagePageInterface {
   readonly pageTitleEdit: (firstName: string, lastName: string) => string;
   readonly pageTitleEditFr: (firstName: string, lastName: string) => string;
+
+  cancel(page: Page): Promise<void>;
 }
