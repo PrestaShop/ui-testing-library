56515c16e082b44d1e8721880ac829b35ca21170
Florine Hea <heaflorine@gmail.com>
2025-01-24 14:55:18 +0100

    fixed lint

--- src/interfaces/BO/advancedParameters/security/customerSessions/index.ts
+++ src/interfaces/BO/advancedParameters/security/customerSessions/index.ts
@@ -1,5 +1,4 @@
 import {BOBasePagePageInterface} from '@interfaces/BO';
-import {type Page} from '@playwright/test';
 
 export interface BOCustomerSessionsPageInterface extends BOBasePagePageInterface {
   readonly pageTitle: string;
