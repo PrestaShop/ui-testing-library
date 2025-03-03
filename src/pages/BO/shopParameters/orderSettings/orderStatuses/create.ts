46b169e9a3f4b1ca573528740bd02600f2f65936
Progi1984 <franck.lefevre@prestashop.com>
2025-03-03 14:28:24 +0100

    Migrate `@pages/BO/shopParameters/orderSettings/statuses/add` from Core

---
+++ src/pages/BO/shopParameters/orderSettings/orderStatuses/create.ts
@@ -0,0 +1,9 @@
+import type {BOOrderStatusesCreatePageInterface} from '@interfaces/BO/shopParameters/orderSettings/orderStatuses/create';
+
+/* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
+function requirePage(): BOOrderStatusesCreatePageInterface {
+  return require('@versions/develop/pages/BO/shopParameters/orderSettings/orderStatuses/create');
+}
+/* eslint-enable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
+
+export default requirePage();
