f226b9a012ea903bdba4b5dd4b467c09548be9bc
Progi1984 <franck.lefevre@prestashop.com>
2025-03-27 10:04:10 +0100

    Migrate `@pages/BO/shopParameters/trafficAndSeo/seoAndUrls/add` from Core

--- src/index.ts
+++ src/index.ts
@@ -318,6 +318,7 @@ export {default as boSearchAliasPage} from '@pages/BO/shopParameters/search/alia
 export {default as boSearchAliasCreatePage} from '@pages/BO/shopParameters/search/alias/create';
 export {default as boSecurityPage} from '@pages/BO/advancedParameters/security';
 export {default as boSeoUrlsPage} from '@pages/BO/shopParameters/trafficAndSeo/seoAndUrls';
+export {default as boSeoUrlsCreatePage} from '@pages/BO/shopParameters/trafficAndSeo/seoAndUrls/create';
 export {default as boShippingPreferencesPage} from '@pages/BO/shipping/preferences';
 export {default as boShopParametersPage} from '@pages/BO/shopParameters/general';
 export {default as boShoppingCartsPage} from '@pages/BO/orders/shoppingCarts';
---
+++ src/pages/BO/shopParameters/trafficAndSeo/seoAndUrls/create.ts
@@ -0,0 +1,9 @@
+import {type BOSeoUrlsCreatePageInterface} from '@interfaces/BO/shopParameters/trafficAndSeo/seoAndUrls/create';
+
+/* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
+function requirePage(): BOSeoUrlsCreatePageInterface {
+  return require('@versions/develop/pages/BO/shopParameters/trafficAndSeo/seoAndUrls/create');
+}
+/* eslint-enable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
+
+export default requirePage();
