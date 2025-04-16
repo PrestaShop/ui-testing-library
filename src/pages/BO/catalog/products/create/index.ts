9284153531a41b0c13e5285665dd663bf3ccd75b
dparis <david.paris@prestashop.com>
2025-04-16 14:39:46 +0200

    new product save button

--- src/pages/BO/catalog/products/create/index.ts
+++ src/pages/BO/catalog/products/create/index.ts
@@ -15,6 +15,9 @@ function requirePage(): BOProductsCreatePageInterface {
   if (semver.lt(psVersion, '7.6.0')) {
     return require('@versions/1.7.5/pages/BO/catalog/products/create').boProductsCreatePage;
   }
+  if (semver.lt(psVersion, '8.0.0')) {
+    return require('@versions/1.7.8/pages/BO/catalog/products/create').boProductsCreatePage;
+  }
   if (semver.lt(psVersion, '8.1.0')) {
     return require('@versions/8.0/pages/BO/catalog/products/create').boProductsCreatePage;
   }
