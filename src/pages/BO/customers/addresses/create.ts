de822e9ed39440fef643ff2644a17faa3796f32b
SD1982 <stephane.decisy@prestashop.com>
2025-04-23 17:13:10 +0200

    Add condition for addresses page

--- src/pages/BO/customers/addresses/create.ts
+++ src/pages/BO/customers/addresses/create.ts
@@ -1,7 +1,14 @@
 import {type BOAddressesCreatePageInterface} from '@interfaces/BO/customers/addresses/create';
+import testContext from '@utils/test';
+import semver from 'semver';
+
+const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BOAddressesCreatePageInterface {
+  if (semver.lt(psVersion, '7.8.0')) {
+    return require('@versions/1.7.7/pages/BO/customers/addresses/create');
+  }
   return require('@versions/develop/pages/BO/customers/addresses/create');
 }
 /* eslint-enable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
