86c5cc3a2f01d05dcd2e0acffc8c510b23982d76
SD1982 <stephane.decisy@prestashop.com>
2025-04-24 14:36:05 +0200

    implement version for addresses index

--- src/pages/BO/customers/addresses/index.ts
+++ src/pages/BO/customers/addresses/index.ts
@@ -1,8 +1,18 @@
 import {type BOAddressesPageInterface} from '@interfaces/BO/customers/addresses';
+import testContext from '@utils/test';
+import semver from 'semver';
+
+const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BOAddressesPageInterface {
-  return require('@versions/develop/pages/BO/customers/addresses');
+  if (semver.lt(psVersion, '7.8.0')) {
+    return require('@versions/1.7.7/pages/BO/customers/addresses').boAddressesPage;
+  }
+  if (semver.lt(psVersion, '9.0.0')) {
+    return require('@versions/8.2/pages/BO/customers/addresses').boAddressesPage;
+  }
+  return require('@versions/develop/pages/BO/customers/addresses').boAddressesPage;
 }
 /* eslint-enable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 
