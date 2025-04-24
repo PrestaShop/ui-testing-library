5c951b72291bf6e96250a441e0b169de177a0d6a
SD1982 <stephane.decisy@prestashop.com>
2025-04-24 10:49:05 +0200

    Fix missing element

--- src/pages/BO/customers/addresses/create.ts
+++ src/pages/BO/customers/addresses/create.ts
@@ -9,7 +9,7 @@ function requirePage(): BOAddressesCreatePageInterface {
   if (semver.lt(psVersion, '7.8.0')) {
     return require('@versions/1.7.7/pages/BO/customers/addresses/create').boAddressesCreatePage;
   }
-  return require('@versions/develop/pages/BO/customers/addresses/create');
+  return require('@versions/develop/pages/BO/customers/addresses/create').boAddressesCreatePage;
 }
 /* eslint-enable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 
