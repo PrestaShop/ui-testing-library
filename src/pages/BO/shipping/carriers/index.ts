0908986db1aab3973d154524f5ec8ea68e393e1e
SD1982 <stephane.decisy@prestashop.com>
2025-04-24 14:13:49 +0200

    Add '.boCarriersPage' to version requirement

--- src/pages/BO/shipping/carriers/index.ts
+++ src/pages/BO/shipping/carriers/index.ts
@@ -7,10 +7,10 @@ const psVersion = testContext.getPSVersion();
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BOCarriersPageInterface {
   if (semver.lt(psVersion, '7.8.0')) {
-    return require('@versions/1.7.7/pages/BO/shipping/carriers');
+    return require('@versions/1.7.7/pages/BO/shipping/carriers').boCarriersPage;
   }
   if (semver.lt(psVersion, '9.0.0')) {
-    return require('@versions/8.2/pages/BO/shipping/carriers');
+    return require('@versions/8.2/pages/BO/shipping/carriers').boCarriersPage;
   }
   return require('@versions/develop/pages/BO/shipping/carriers').boCarriersPage;
 }
