3e98f8779ae15896b686b8ef56d23854f7c82ef7
SD1982 <stephane.decisy@prestashop.com>
2025-04-25 14:56:00 +0200

    Move order setPaymentMethod override from 1.7.7 to 1.7.8

--- src/pages/BO/orders/create.ts
+++ src/pages/BO/orders/create.ts
@@ -9,6 +9,9 @@ function requirePage(): BOOrdersCreatePageInterface {
   if (semver.lt(psVersion, '7.8.0')) {
     return require('@versions/1.7.7/pages/BO/orders/create').boOrderCreatePage;
   }
+  if (semver.lt(psVersion, '8.0.0')) {
+    return require('@versions/1.7.8/pages/BO/orders/create').boOrderCreatePage;
+  }
   if (semver.lt(psVersion, '9.0.0')) {
     return require('@versions/8.2/pages/BO/orders/create').boOrderCreatePage;
   }
