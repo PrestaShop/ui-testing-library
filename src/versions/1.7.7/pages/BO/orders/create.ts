729179be1d871c70c56d872d262ef06e502b1c4e
SD1982 <stephane.decisy@prestashop.com>
2025-04-25 10:09:59 +0200

    Remove useless override

--- src/versions/1.7.7/pages/BO/orders/create.ts
+++ src/versions/1.7.7/pages/BO/orders/create.ts
@@ -13,46 +13,6 @@ class BOOrderCreatePage extends BOOrderCreatePageVersion implements BOOrdersCrea
     constructor() {
         super();
     }
-
-    /**
-     * Create order with existing customer
-     * @param page {Page} Browser tab
-     * @param orderToMake {FakerOrder} Order data to create
-     * @param isNewCustomer {boolean} True if the customer is new
-     * @returns {Promise<void>}
-     */
-    async createOrder(page: Page, orderToMake: FakerOrder, isNewCustomer: boolean = false): Promise<void> {
-        // Choose customer
-        // If it's a new customer, the creation of customer should be done in test
-        // with add customer page
-        if (!isNewCustomer) {
-            await this.searchCustomer(page, orderToMake.customer.email);
-        }
-
-        // Choose customer after search or creation
-        await this.chooseCustomer(page, 1);
-
-        // Add products to carts
-        for (let i = 0; i < orderToMake.products.length; i++) {
-            await this.addProductToCart(
-                page, orderToMake.products[i].product, orderToMake.products[i].product.name, orderToMake.products[i].quantity);
-        }
-
-        // Choose address
-        await this.chooseAddresses(page, orderToMake.deliveryAddress.alias, orderToMake.invoiceAddress.alias);
-
-        // Choose delivery options
-        await this.setDeliveryOption(page, orderToMake.deliveryOption.name, orderToMake.deliveryOption.freeShipping);
-
-        // Choose payment method
-        await this.setPaymentMethod(page, orderToMake.paymentMethod.moduleName);
-
-        // Set order status
-        await this.setOrderStatus(page, orderToMake.status);
-
-        // Create the order
-        await this.clickAndWaitForURL(page, this.createOrderButton);
-    }
 }
 
 const boOrderCreatePage = new BOOrderCreatePage();
