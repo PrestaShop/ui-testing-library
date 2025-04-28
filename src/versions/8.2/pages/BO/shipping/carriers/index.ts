3a9c40d3bb26fac5975b176bb578b8dae0b8efef
SD1982 <stephane.decisy@prestashop.com>
2025-04-28 10:44:58 +0200

    Add login compatibility for 1.6.1.24

--- src/versions/8.2/pages/BO/shipping/carriers/index.ts
+++ src/versions/8.2/pages/BO/shipping/carriers/index.ts
@@ -89,7 +89,7 @@ class BOCarriersPage extends BOCarriersPageVersion implements BOCarriersPageInte
   /**
    * Get text from column in table
    * @param page {Page} Browser tab
-   * @param row {number} Row index in the table
+   * @param row {number} Row index.ts in the table
    * @param columnName {string} Column name in the table
    * @return {Promise<string>}
    */
@@ -168,7 +168,7 @@ class BOCarriersPage extends BOCarriersPageVersion implements BOCarriersPageInte
   /**
    * Set free shipping status
    * @param page {Page} Browser tab
-   * @param row {number} Row index. in the table
+   * @param row {number} Row index in the table
    * @param valueWanted {boolean} The carrier status value
    * @return {Promise<boolean>}
    */
