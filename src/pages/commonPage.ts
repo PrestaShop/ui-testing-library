fca4e82bfbc5838532ee2460e2f973b3a5ee008a
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2025-04-28 22:38:57 +0200

    Add optional clickOptions parameter

--- src/pages/commonPage.ts
+++ src/pages/commonPage.ts
@@ -432,6 +432,7 @@ export default class CommonPage implements CommonPageInterface {
    * @param selector {string} String to locate the element
    * @param state {'load'|'domcontentloaded'|'networkidle'} The event to wait after click
    * @param timeout {number} Time to wait for navigation
+   * @param clickOptions {any} Click options
    * @return {Promise<void>}
    */
   async clickAndWaitForLoadState(
@@ -439,10 +440,11 @@ export default class CommonPage implements CommonPageInterface {
     selector: string,
     state: 'load' | 'domcontentloaded' | 'networkidle' = 'load',
     timeout: number = 30000,
+    clickOptions: any = {},
   ): Promise<void> {
     await Promise.all([
       page.waitForLoadState(state, {timeout}),
-      page.locator(selector).click(),
+      page.locator(selector).click(clickOptions),
     ]);
   }
 
@@ -452,6 +454,7 @@ export default class CommonPage implements CommonPageInterface {
    * @param selector {string} String to locate the element
    * @param waitUntil {WaitForNavigationWaitUntil} The event to wait after click
    * @param timeout {number} Time to wait for navigation
+   * @param clickOptions {any} Click options
    * @return {Promise<void>}
    */
   async clickAndWaitForURL(
@@ -459,12 +462,13 @@ export default class CommonPage implements CommonPageInterface {
     selector: string,
     waitUntil: WaitForNavigationWaitUntil = 'load',
     timeout: number = 30000,
+    clickOptions: any = {},
   ): Promise<void> {
     const currentUrl: string = page.url();
 
     await Promise.all([
       page.waitForURL((url: URL): boolean => url.toString() !== currentUrl, {waitUntil, timeout}),
-      page.locator(selector).first().click(),
+      page.locator(selector).click(clickOptions),
     ]);
   }
 
