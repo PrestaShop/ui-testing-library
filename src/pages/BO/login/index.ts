e1d16e4b6bf11c7f13961156cb547af7b0f26e48
SD1982 <stephane.decisy@prestashop.com>
2025-04-28 15:09:12 +0200

    Remove temporary login require

--- src/pages/BO/login/index.ts
+++ src/pages/BO/login/index.ts
@@ -6,9 +6,6 @@ const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): LoginPageInterface {
-  if (semver.lt(psVersion, '7.0.0')) {
-    return require('@versions/1.6.1/pages/BO/login').loginPage;
-  }
   if (semver.lt(psVersion, '7.3.0')) {
     return require('@versions/1.7.2/pages/BO/login').loginPage;
   }
