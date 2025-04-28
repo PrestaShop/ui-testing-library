56c996e0633dba0f175a719b86ade74740269ada
SD1982 <stephane.decisy@prestashop.com>
2025-04-28 15:00:38 +0200

    Remove temporary carriersCreate

--- src/versions/develop/pages/BO/dashboard/index.ts
+++ src/versions/develop/pages/BO/dashboard/index.ts
@@ -757,4 +757,5 @@ class Dashboard extends BOBasePage implements DashboardPageInterface {
   }
 }
 
-module.exports = new Dashboard();
+const DashboardPage = new Dashboard();
+export {DashboardPage, Dashboard};
