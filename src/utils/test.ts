1b60b152ddaa97899de82527800c1ab29a29ea5a
SD1982 <stephane.decisy@prestashop.com>
2025-04-28 14:21:18 +0200

    Add version to utils

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -50,7 +50,8 @@ export default {
     const version: string = process.env.PS_VERSION;
 
     return version
-      .replace(/\.x$/, '.99')
-      .replace(/^1\.7\./, '7.');
+        .replace(/\.x$/, '.99')
+        .replace(/^1\.7\./, '7.')
+        .replace(/^1\.6\./, '6.');
   },
 };
