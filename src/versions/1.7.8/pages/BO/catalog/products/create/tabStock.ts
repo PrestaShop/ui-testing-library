bf9b16d33d792db17e99bdc03ad688734b9eacf0
Aurelien Rita <aurelien.rita@prestashop.com>
2025-05-07 17:26:05 +0200

    Change locator In Stock in v1.7.8

---
+++ src/versions/1.7.8/pages/BO/catalog/products/create/tabStock.ts
@@ -0,0 +1,27 @@
+// Import pages
+import type {BOProductsCreateTabStocksPageInterface} from '@interfaces/BO/catalog/products/create/tabStocks';
+import {StocksTab} from '@versions/8.0/pages/BO/catalog/products/create/tabStocks';
+
+/**
+ * Bo product stocks tab, contains functions that can be used on the page
+ * @class
+ * @extends StocksTab
+ */
+class BOProductTabStocksVersion extends StocksTab implements BOProductsCreateTabStocksPageInterface {
+  protected labelWhenInStockInput: string;
+
+  protected labelWhenOutOfStock: string;
+
+  /**
+   * @constructs
+   * Setting up texts and selectors to use on stocks tab
+   */
+  constructor() {
+    super();
+    this.labelWhenInStockInput = '#form_step3_available_now_2';
+    this.labelWhenOutOfStock = '#form_step3_available_later_1';
+  }
+}
+
+const stocksTab = new BOProductTabStocksVersion();
+export {stocksTab, BOProductTabStocksVersion as StocksTab};
