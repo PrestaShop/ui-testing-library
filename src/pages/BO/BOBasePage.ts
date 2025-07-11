cd4cbdafb01cbb71acb6055981f51d6af9954116
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2025-07-11 08:27:38 +0200

    Fix some issues

--- src/pages/BO/BOBasePage.ts
+++ src/pages/BO/BOBasePage.ts
@@ -1015,14 +1015,13 @@ export default class BOBasePage extends CommonPage implements BOBasePagePageInte
     const args = {selector: iFrameSelector, vl: value, hasP: hasParagraph};
     // eslint-disable-next-line no-eval
     const fn: { fnSetValueOnTinymceInput: PageFunction<{ selector: string, vl: string, hasP: boolean }, void> } = eval(`({
-			async fnSetValueOnTinymceInput(args) {
-				/* eslint-env browser */
-				const iFrameElement = await document.querySelector(args.selector);
-				const iFrameHtml = iFrameElement.contentDocument.documentElement;
-				const textElement = await iFrameHtml.querySelector(args.hasP ? 'body p' : 'body');
-				textElement.textContent = args.vl;
-			}
-		})`);
+    async fnSetValueOnTinymceInput(args) {
+      /* eslint-env browser */
+      const iFrameElement = await document.querySelector(args.selector);
+      const iFrameHtml = iFrameElement.contentDocument.documentElement;
+      const textElement = await iFrameHtml.querySelector(args.hasP ? 'body p' : 'body');
+      textElement.textContent = args.vl;
+    }})`);
     await page.evaluate(fn.fnSetValueOnTinymceInput, args);
   }
 
@@ -1048,15 +1047,14 @@ export default class BOBasePage extends CommonPage implements BOBasePagePageInte
     const args = {selector, value, onChange};
     // eslint-disable-next-line no-eval
     const fn: { fnSetValueOnDTPickerInput: PageFunction<{ selector: string, value: string, onChange: boolean }, void> } = eval(`({
-			async fnSetValueOnDTPickerInput(args) {
-				/* eslint-env browser */
-				const textElement = await document.querySelector(args.selector);
-				textElement.value = args.value;
-				if (args.onChange) {
-					textElement.dispatchEvent(new Event('change'));
-				}
-			}
-		})`);
+    async fnSetValueOnDTPickerInput(args) {
+      /* eslint-env browser */
+      const textElement = await document.querySelector(args.selector);
+      textElement.value = args.value;
+      if (args.onChange) {
+        textElement.dispatchEvent(new Event('change'));
+      }
+		}})`);
     await page.evaluate(fn.fnSetValueOnDTPickerInput, args);
   }
 
--- src/pages/BO/advancedParameters/featureFlag.ts
+++ src/pages/BO/advancedParameters/featureFlag.ts
@@ -6,10 +6,10 @@ const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BOFeatureFlagInterface {
-  if (semver.lte(psVersion, '8.3.0')) {
-    return require('@versions/8.2.0/pages/BO/advancedParameters/featureFlag').featureFlagPage;
+  if (semver.lt(psVersion, '8.3.0')) {
+    return require('@versions/8.2/pages/BO/advancedParameters/featureFlag');
   }
-  return require('@versions/develop/pages/BO/advancedParameters/featureFlag').featureFlagPage;
+  return require('@versions/develop/pages/BO/advancedParameters/featureFlag');
 }
 /* eslint-enable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 
