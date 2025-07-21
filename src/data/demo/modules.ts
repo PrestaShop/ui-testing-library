5b3e14cfeed2ab5cb6edfb622c0d3dac1fbff74f
Progi1984 <Progi1984@users.noreply.github.com>
2025-07-21 23:02:25 +0000

    `@data/demo/modules`: Update modules

--- src/data/demo/modules.ts
+++ src/data/demo/modules.ts
@@ -73,7 +73,7 @@ export default {
     tag: 'ps_newproducts',
     name: 'New products block',
     versionCurrent: 'v1.0.5',
-    versionOld: 'v1.0.4',
+    versionOld: 'v1.0.5',
     releaseZip: 'https://github.com/PrestaShop/ps_newproducts/releases/download/%version%/ps_newproducts.zip',
   }),
   psSupplierList: new FakerModule({
