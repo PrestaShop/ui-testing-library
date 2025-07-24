b4b867024ebedd44435d8cba9896a1f2709c32e6
SD1982 <stephane.decisy@prestashop.com>
2025-07-24 11:14:56 +0200

    Remove click timeout

--- src/pages/BO/BOBasePage.ts
+++ src/pages/BO/BOBasePage.ts
@@ -752,7 +752,7 @@ export default class BOBasePage extends CommonPage implements BOBasePagePageInte
     if (semver.lt(shopVersion, '7.4.0')) {
       await page.hover(parentSelector);
       //await this.clickAndWaitForURL(page, linkSelector);
-      await page.locator(linkSelector).click({timeout: 1500});
+      await page.locator(linkSelector).click();
       await page.waitForLoadState("load");
     } else {
       if (parentSelector !== '') {
@@ -760,7 +760,7 @@ export default class BOBasePage extends CommonPage implements BOBasePagePageInte
         await this.scrollTo(page, linkSelector);
       }
       //await this.clickAndWaitForURL(page, linkSelector);
-      await page.locator(linkSelector).click({timeout: 1500});
+      await page.locator(linkSelector).click();
       await page.waitForLoadState("load");
       let linkActiveClass: string = '-active';
 
