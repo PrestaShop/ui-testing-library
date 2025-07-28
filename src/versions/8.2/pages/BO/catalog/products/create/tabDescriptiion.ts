62acc4b36a6f66f29537717ce466f3cbb93e28f8
SD1982 <stephane.decisy@prestashop.com>
2025-07-28 16:33:29 +0200

    Edit productDescription

--- src/versions/8.2/pages/BO/catalog/products/create/tabDescriptiion.ts
+++ src/versions/8.2/pages/BO/catalog/products/create/tabDescriptiion.ts
@@ -1,6 +1,6 @@
 // Import pages
 import type {BOProductsCreateTabDescriptionPageInterface} from '@interfaces/BO/catalog/products/create/tabDescription';
-import {DescriptionTab} from '@versions/8.0/pages/BO/catalog/products/create/tabDescription';
+import {DescriptionTab} from '@versions/develop/pages/BO/catalog/products/create/tabDescription';
 
 import type FakerProduct from '@data/faker/product';
 
@@ -12,6 +12,9 @@ import type {Page} from 'playwright';
  * @extends DescriptionTab
  */
 class BOProductTabDescriptionVersion extends DescriptionTab implements BOProductsCreateTabDescriptionPageInterface {
+    private readonly productShortDescriptionIframe: string;
+    private readonly productDescriptionIframe: string;
+    private productWithCombinationsInput: string;
     /**
      * @constructs
      * Setting up texts and selectors to use on description tab
@@ -22,6 +25,7 @@ class BOProductTabDescriptionVersion extends DescriptionTab implements BOProduct
         this.descriptionTabLink = '#product_description-tab-nav';
         this.productShortDescriptionIframe = '#product_description_description_short';
         this.productDescriptionIframe = '#product_description_description';
+        this.productWithCombinationsInput = '#show_variations_selector div:nth-of-type(2) input';
     }
 
     /**
