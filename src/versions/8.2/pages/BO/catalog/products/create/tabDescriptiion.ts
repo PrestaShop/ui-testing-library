db9173a8dc3ed7fd0c007beb4cd97d013011e634
SD1982 <stephane.decisy@prestashop.com>
2025-07-28 15:51:36 +0200

    Edit setProductDescription for 8.2.1

---
+++ src/versions/8.2/pages/BO/catalog/products/create/tabDescriptiion.ts
@@ -0,0 +1,47 @@
+// Import pages
+import type {BOProductsCreateTabDescriptionPageInterface} from '@interfaces/BO/catalog/products/create/tabDescription';
+import {DescriptionTab} from '@versions/8.0/pages/BO/catalog/products/create/tabDescription';
+
+import type FakerProduct from '@data/faker/product';
+
+import type {Page} from 'playwright';
+
+/**
+ * Bo product description tab, contains functions that can be used on the page
+ * @class
+ * @extends DescriptionTab
+ */
+class BOProductTabDescriptionVersion extends DescriptionTab implements BOProductsCreateTabDescriptionPageInterface {
+    /**
+     * @constructs
+     * Setting up texts and selectors to use on description tab
+     */
+    constructor() {
+        super();
+
+        this.descriptionTabLink = '#product_description-tab-nav';
+        this.productShortDescriptionIframe = '#product_description_description_short';
+        this.productDescriptionIframe = '#product_description_description';
+    }
+
+    /**
+     * Set product description
+     * @param page {Page} Browser tab
+     * @param productData {FakerProduct} Data to set in description form
+     * @returns {Promise<void>}
+     */
+    async setProductDescription(page: Page, productData: FakerProduct): Promise<void> {
+        await this.waitForSelectorAndClick(page, this.descriptionTabLink);
+        if (productData.type === 'combinations') {
+            await page.locator(this.productWithCombinationsInput).click();
+        }
+
+        await this.addProductImages(page, [productData.coverImage, productData.thumbImage]);
+
+        await this.setValueOnTinymceInput(page, this.productDescriptionIframe, productData.description);
+        await this.setValueOnTinymceInput(page, this.productShortDescriptionIframe, productData.summary);
+    }
+}
+
+const descriptionTab = new BOProductTabDescriptionVersion();
+export {descriptionTab, BOProductTabDescriptionVersion};
