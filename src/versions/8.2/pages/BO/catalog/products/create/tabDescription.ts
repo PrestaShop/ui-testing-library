bf84db75da5fdbcfb1cf9047dc2eaf72f09fe5f8
SD1982 <stephane.decisy@prestashop.com>
2025-07-29 09:42:02 +0200

    Remove function from 8.2

--- src/versions/8.2/pages/BO/catalog/products/create/tabDescription.ts
+++ src/versions/8.2/pages/BO/catalog/products/create/tabDescription.ts
@@ -27,24 +27,6 @@ class BOProductTabDescriptionVersion extends DescriptionTab implements BOProduct
         this.productDescriptionIframe = '#product_description_description';
         this.productWithCombinationsInput = '#show_variations_selector div:nth-of-type(2) input';
     }
-
-    /**
-     * Set product description
-     * @param page {Page} Browser tab
-     * @param productData {FakerProduct} Data to set in description form
-     * @returns {Promise<void>}
-     */
-    async setProductDescription(page: Page, productData: FakerProduct): Promise<void> {
-        await this.waitForSelectorAndClick(page, this.descriptionTabLink);
-        if (productData.type === 'combinations') {
-            await page.locator(this.productWithCombinationsInput).click();
-        }
-
-        await this.addProductImages(page, [productData.coverImage, productData.thumbImage]);
-
-        await this.setValueOnTinymceInput(page, this.productDescriptionIframe, productData.description);
-        await this.setValueOnTinymceInput(page, this.productShortDescriptionIframe, productData.summary);
-    }
 }
 
 const descriptionTab = new BOProductTabDescriptionVersion();
