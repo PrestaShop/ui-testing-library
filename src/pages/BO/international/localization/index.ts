4faeaf7b581dcd438eca2600d585863625e5826c
SD1982 <stephane.decisy@prestashop.com>
2025-08-12 15:12:34 +0200

    Add selector for localization button import

--- src/pages/BO/international/localization/index.ts
+++ src/pages/BO/international/localization/index.ts
@@ -1,7 +1,14 @@
 import {type BOLocalizationPageInterface} from '@interfaces/BO/international/localization';
+import testContext from '@utils/test';
+import semver from 'semver';
+
+const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BOLocalizationPageInterface {
+    if (semver.lt(psVersion, '1.7.8')) {
+        return require('@versions/1.7/pages/BO/international/localization').bo;
+    }
   return require('@versions/develop/pages/BO/international/localization');
 }
 /* eslint-enable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
---
+++ src/versions/1.7.7/pages/BO/international/localization/index.ts
@@ -0,0 +1,22 @@
+import {type BOLocalizationPageInterface} from '@interfaces/BO/international/localization';
+import {BOLocalizationPage as BOLocalizationPageVersion} from '@versions/develop/pages/BO/international/localization';
+
+/**
+ * Addresses page, contains functions that can be used on the page
+ * @class
+ * @extends BOBasePage
+ */
+class BOLocalizationPage extends BOLocalizationPageVersion implements BOLocalizationPageInterface {
+    /**
+     * @constructs
+     * Setting up texts and selectors to use on addresses page
+     */
+    constructor() {
+        super();
+
+        this.importButton = 'button.btn.btn-primary:has-text("Import")';
+    }
+}
+
+const boLocalizationPage = new BOLocalizationPage();
+export {boLocalizationPage, BOLocalizationPage};
