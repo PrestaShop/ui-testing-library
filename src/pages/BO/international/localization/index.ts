5a2700168045f82b695ae618217324a843423a0f
SD1982 <stephane.decisy@prestashop.com>
2025-08-12 15:54:12 +0200

    Fix another typo

--- src/pages/BO/international/localization/index.ts
+++ src/pages/BO/international/localization/index.ts
@@ -7,7 +7,7 @@ const psVersion = testContext.getPSVersion();
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BOLocalizationPageInterface {
     if (semver.lt(psVersion, '7.8.0')) {
-        return require('@versions/1.7/pages/BO/international/localization').boLocalizationPage;
+        return require('@versions/1.7.7/pages/BO/international/localization').boLocalizationPage;
     }
   return require('@versions/develop/pages/BO/international/localization');
 }
