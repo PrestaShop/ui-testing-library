5b4eadc981a2bda8abe9d886218d5e43825f6220
SD1982 <stephane.decisy@prestashop.com>
2025-08-13 14:36:33 +0200

    Update 1.6 locators for localization

---
+++ src/versions/1.6.1/pages/BO/localization/localization/index.ts
@@ -0,0 +1,31 @@
+import {type BOLocalizationPageInterface} from '@interfaces/BO/international/localization';
+import {BOLocalizationPage as BOLocalizationPageVersion} from '@versions/1.7.7/pages/BO/international/localization';
+
+/**
+ * Addresses page, contains functions that can be used on the page
+ * @class
+ * @extends BOBasePage
+ */
+class BOLocalizationPage extends BOLocalizationPageVersion implements BOLocalizationPageInterface {
+    /**
+     * @constructs
+     * Setting up texts and selectors to use on addresses page
+     */
+    constructor() {
+        super();
+
+        // Import localization pack selectors
+        this.importlocalizationPackSelect = 'select#iso_localization_pack';
+        this.importStatesCheckbox = '#selection[]_states';
+        this.importTaxesCheckbox = '#selection[]_taxes';
+        this.importCurrenciesCheckbox = '#selection[]_currencies';
+        this.importLanguagesCheckbox = '#selection[]_languages';
+        this.importUnitsCheckbox = '#selection[]_units';
+        this.updatepriceDisplayForGroupsCHeckbox = '#selection[]_groups';
+        this.downloadPackDataToggleInput = (toggle: number) => `#download_updated_pack_${toggle}`;
+        this.importButton = 'button.btn.btn-primary:has-text("Import")';
+    }
+}
+
+const boLocalizationPage = new BOLocalizationPage();
+export {boLocalizationPage, BOLocalizationPage};
