de0fe3680ecadc3a2b7cab95a06cfa2ed90a1f04
SD1982 <stephane.decisy@prestashop.com>
2025-08-13 15:43:58 +0200

    Replace input locator by textbox

--- src/versions/1.6.1/pages/BO/localization/localization/index.ts
+++ src/versions/1.6.1/pages/BO/localization/localization/index.ts
@@ -45,7 +45,7 @@ class BOLocalizationPage extends BOLocalizationPageVersion implements BOLocaliza
     ): Promise<string> {
         // Choose which country to import
         await page.locator('#iso_localization_pack_chosen').click();
-        await page.locator('div.chosen-search input').fill(country);
+        await page.locator('#iso_localization_pack_chosen').getByRole('textbox').fill(country);
         await page.locator('ul.chosen-results li').filter({hasText: country}).click();
 
         // Set content import checkboxes
