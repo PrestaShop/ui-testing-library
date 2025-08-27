10e7e3d86f227a32e838183663ac6551c1a40713
SD1982 <stephane.decisy@prestashop.com>
2025-08-27 09:30:57 +0200

    Second overide for carriers creation

--- src/versions/1.7.7/pages/BO/shipping/carriers/create.ts
+++ src/versions/1.7.7/pages/BO/shipping/carriers/create.ts
@@ -4,6 +4,10 @@ import {BOCarriersCreatePage as BOCarriersCreatePageVersion} from '@versions/8.2
 class BOCarriersCreatePage extends BOCarriersCreatePageVersion implements BOCarriersCreatePageInterface {
     constructor() {
         super();
+
+        // Shipping locations and costs
+        this.addHandlingCostsToggle = (toggle: string) => `#step_carrier_ranges #shipping_handling_${toggle}`;
+        this.freeShippingToggle = (toggle: string) => `#step_carrier_ranges #is_free_${toggle}`;
         // Summary
         this.enableToggle = (toggle: string) => `#step_carrier_summary #active_${toggle}`;
     }
