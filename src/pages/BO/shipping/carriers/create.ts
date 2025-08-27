955fc46894e4f9d1093d0edec881e434f05de687
SD1982 <stephane.decisy@prestashop.com>
2025-08-27 09:26:45 +0200

    Overide locator for carrier enable in 1.7.7

--- src/pages/BO/shipping/carriers/create.ts
+++ src/pages/BO/shipping/carriers/create.ts
@@ -6,6 +6,9 @@ const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BOCarriersCreatePageInterface {
+  if (semver.lt(psVersion, '7.8.0')) {
+    return require('@versions/1.7.7/pages/BO/shipping/carriers/create').boCarriersCreatePage;
+  }
   if (semver.lt(psVersion, '9.0.0')) {
     return require('@versions/8.2/pages/BO/shipping/carriers/create').boCarriersCreatePage;
   }
