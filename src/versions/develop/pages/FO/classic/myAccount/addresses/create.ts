b9e608269250045a4404fa83e4139450e7039ee0
Progi1984 <franck.lefevre@prestashop.com>
2025-11-03 20:05:14 +0100

    `foClassicMyAddressesCreatePage` : Added support for state

--- src/versions/develop/pages/FO/classic/myAccount/addresses/create.ts
+++ src/versions/develop/pages/FO/classic/myAccount/addresses/create.ts
@@ -37,6 +37,8 @@ class FoMyAddressesCreatePage extends FOBasePage implements FoMyAddressesCreateP
 
   private readonly cityInput: string;
 
+  private readonly stateSelect: string;
+
   private readonly countrySelect: string;
 
   private readonly phoneInput: string;
@@ -66,6 +68,7 @@ class FoMyAddressesCreatePage extends FOBasePage implements FoMyAddressesCreateP
     this.secondAddressInput = `${this.addressForm} input[name=address2]`;
     this.postCodeInput = `${this.addressForm} input[name=postcode]`;
     this.cityInput = `${this.addressForm} input[name=city]`;
+    this.stateSelect = `${this.addressForm} select[name=id_state]`;
     this.countrySelect = `${this.addressForm} select[name=id_country]`;
     this.phoneInput = `${this.addressForm} input[name=phone]`;
     this.saveButton = `${this.addressForm} input[name=submitAddress] + button`;
@@ -119,6 +122,9 @@ class FoMyAddressesCreatePage extends FOBasePage implements FoMyAddressesCreateP
     await this.setValue(page, this.postCodeInput, addressData.postalCode);
     await this.setValue(page, this.cityInput, addressData.city);
     await this.selectByVisibleText(page, this.countrySelect, addressData.country);
+    if (addressData.state) {
+      await this.selectByVisibleText(page, this.stateSelect, addressData.state);
+    }
 
     // Set phone if added (optional)
     if (addressData.phone) {
