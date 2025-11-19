b4defeeb37f93234297534bbf0cad4c7f4818ce0
Florine Hea <heaflorine@gmail.com>
2025-11-19 16:49:44 +0100

    edited for review

--- src/versions/1.7.7/pages/FO/classic/checkout/index.ts
+++ src/versions/1.7.7/pages/FO/classic/checkout/index.ts
@@ -24,40 +24,6 @@ class CheckoutPage extends FOBasePage {
 
   public readonly authenticationErrorMessage: string;
 
-  protected paymentStepSection: string;
-
-  protected paymentOptionInput: (name: string) => string;
-
-  protected paymentOptionAlertDanger: string;
-
-  protected conditionToApproveLabel: string;
-
-  protected shippingValueSpan: string;
-
-  protected discountValueSpan: string;
-
-  protected cartSummaryLine: (line: number) => string;
-
-  protected cartRuleName: (line: number) => string;
-
-  protected itemsNumber: string;
-
-  protected showDetailsLink: string;
-
-  private readonly productList: string;
-
-  protected productRowLink: (productRow: number) => string;
-
-  protected productDetailsImage: (productRow: number) => string;
-
-  protected productDetailsName: (productRow: number) => string;
-
-  protected productDetailsQuantity: (productRow: number) => string;
-
-  protected productDetailsPrice: (productRow: number) => string;
-
-  protected productDetailsAttributes: (productRow: number) => string;
-
   public readonly noPaymentNeededText: string;
 
   public personalInformationStepForm: string;
@@ -65,12 +31,6 @@ class CheckoutPage extends FOBasePage {
   protected forgetPasswordLink: string;
 
   protected signInHyperLink: string;
-
-  protected checkoutSummary: string;
-
-  protected checkoutPromoBlock: string;
-
-  protected checkoutRemoveDiscountLink: (row: number) => string;
 
   protected checkoutLoginForm: string;
 
@@ -112,33 +72,11 @@ class CheckoutPage extends FOBasePage {
 
   protected deliveryStepSection: string;
 
-  protected deliveryStepEditButton: string;
-
-  protected deliveryOptions: string;
-
   protected deliveryOptionLabel: (id: number) => string;
-
-  protected deliveryOptionAllNamesSpan: string;
 
   private readonly deliveryMessage: string;
 
   private readonly deliveryStepContinueButton: string;
-
-  protected deliveryOption: (carrierID: number) => string;
-
-  protected deliveryStepCarrierName: (carrierID: number) => string;
-
-  protected deliveryStepCarrierDelay: (carrierID: number) => string;
-
-  protected deliveryStepCarrierPrice: (carrierID: number) => string;
-
-  protected deliveryAddressPosition: (position: number) => string;
-
-  protected invoiceAddressPosition: (position: number) => string;
-
-  protected deliveryAddressEditButton: (addressID: number) => string;
-
-  protected deliveryAddressDeleteButton: (addressID: number) => string;
 
   /**
    * @constructs
@@ -182,58 +120,12 @@ class CheckoutPage extends FOBasePage {
     this.addressStepEditButton = `${this.addressStepSection} span.step-edit`;
     this.addAddressButton = '#checkout-addresses-step p.add-address a';
     this.addInvoiceAddressButton = '#checkout-addresses-step  p.add-address a[href*="invoice"]';
-    // Delivery address selectors
-    this.deliveryAddressEditButton = (addressID: number) => `#id_address_delivery-address-${addressID} a.edit-address`;
-    this.deliveryAddressDeleteButton = (addressID: number) => `#id_address_delivery-address-${addressID} a.delete-address`;
-      + 'input[name="id_address_delivery"]';
-    // Invoice address selectors
-    this.deliveryAddressPosition = (position: number) => `#delivery-addresses article:nth-child(${position})`;
-    this.invoiceAddressPosition = (position: number) => `#invoice-addresses article:nth-child(${position})`;
-      + ' input[name="id_address_invoice"]';
 
     // Shipping method selectors
     this.deliveryStepSection = '#checkout-delivery-step';
-    this.deliveryStepEditButton = `${this.deliveryStepSection} span.step-edit`;
-    this.deliveryOptions = '#js-delivery div.delivery-options';
     this.deliveryOptionLabel = (id: number) => `${this.deliveryStepSection} label[for='delivery_option_${id}']`;
-    this.deliveryOptionAllNamesSpan = '#js-delivery .delivery-option .carriere-name-container span.carrier-name';
     this.deliveryMessage = '#delivery_message';
     this.deliveryStepContinueButton = `${this.deliveryStepSection} button[name='confirmDeliveryOption']`;
-    this.deliveryOption = (carrierID: number) => `${this.deliveryOptions} label[for=delivery_option_${carrierID}] span.carrier`;
-    this.deliveryStepCarrierName = (carrierID: number) => `${this.deliveryOption(carrierID)}-name`;
-    this.deliveryStepCarrierDelay = (carrierID: number) => `${this.deliveryOption(carrierID)}-delay`;
-    this.deliveryStepCarrierPrice = (carrierID: number) => `${this.deliveryOption(carrierID)}-price`;
-
-    // Payment step selectors
-    this.paymentStepSection = '#checkout-payment-step';
-    this.paymentOptionInput = (name: string) => `${this.paymentStepSection} input[name='payment-option']`
-      + `[data-module-name='${name}']`;
-    this.paymentOptionAlertDanger = '.payment-options p.alert-danger';
-    this.conditionToApproveLabel = `${this.paymentStepSection} #conditions-to-approve label`;
-
-    // Checkout summary selectors
-    this.checkoutSummary = '#js-checkout-summary';
-    this.checkoutPromoBlock = `${this.checkoutSummary} div.block-promo`;
-    this.checkoutRemoveDiscountLink = (row: number) => `li.cart-summary-line:nth-child(${row})`
-      + ' a[data-link-action="remove-voucher"] i';
-    this.shippingValueSpan = '#cart-subtotal-shipping span.value';
-    this.discountValueSpan = '#cart-subtotal-discount span.value';
-    this.cartSummaryLine = (line: number) => `${this.checkoutPromoBlock} li:nth-child(${line}).cart-summary-line`;
-    this.cartRuleName = (line: number) => `${this.cartSummaryLine(line)} span.label`;
-
-    // Cart details selectors
-    this.itemsNumber = `${this.checkoutSummary} div.cart-summary-products.js-cart-summary-products p:nth-child(1)`;
-    this.showDetailsLink = `${this.checkoutSummary} div.cart-summary-products.js-cart-summary-products a.js-show-details`;
-    this.productList = '#cart-summary-product-list';
-    this.productRowLink = (productRow: number) => `${this.productList} ul li:nth-child(${productRow})`;
-    this.productDetailsImage = (productRow: number) => `${this.productRowLink(productRow)} div.media-left a img`;
-    this.productDetailsName = (productRow: number) => `${this.productRowLink(productRow)} div span.product-name`;
-    this.productDetailsQuantity = (productRow: number) => `${this.productRowLink(productRow)} `
-      + 'div.media-body span.product-quantity';
-    this.productDetailsPrice = (productRow: number) => `${this.productRowLink(productRow)} div.media-body `
-      + 'span.product-price.float-xs-right';
-    this.productDetailsAttributes = (productRow: number) => `${this.productRowLink(productRow)} div.media-body `
-      + 'div.product-line-info';
   }
 
   /*
