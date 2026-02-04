6aa5b7e85d4d785dfbe81134b909c24c3b799486
Progi1984 <franck@lefevre.dev>
2026-02-04 09:11:51 +0100

    9.2 : Migrate Tag page

--- src/pages/BO/shopParameters/search/tags/create.ts
+++ src/pages/BO/shopParameters/search/tags/create.ts
@@ -1,8 +1,15 @@
 import type {BOTagsCreatePageInterface} from '@interfaces/BO/shopParameters/search/tags/create';
+import testContext from '@utils/test';
+import semver from 'semver';
+
+const psVersion = testContext.getPSVersion();
 
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BOTagsCreatePageInterface {
-  return require('@versions/develop/pages/BO/shopParameters/search/tags/create');
+  if (semver.lt(psVersion, '9.2.0')) {
+    return require('@versions/9.1/pages/BO/shopParameters/search/tags/create').boTagsCreatePage;
+  }
+  return require('@versions/develop/pages/BO/shopParameters/search/tags/create').boTagsCreatePage;
 }
 /* eslint-enable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 
