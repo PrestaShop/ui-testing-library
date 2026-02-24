99a446d5c5fe13a0ad545237b55994e0cea1afc3
Progi1984 <franck.lefevre@prestashop.com>
2026-02-24 16:59:02 +0100

    `modPsGdprBoMain` : Added method parseCSVFile

---
+++ src/data/types/gdpr.ts
@@ -0,0 +1,8 @@
+type GDPRExportCSV = {
+  title: string,
+  headers: string[],
+  rows: string[][],
+  error: string,
+};
+
+export default GDPRExportCSV;
--- src/interfaces/BO/modules/psgdpr/index.ts
+++ src/interfaces/BO/modules/psgdpr/index.ts
@@ -1,3 +1,4 @@
+import type GDPRExportCSV from '@data/types/gdpr';
 import {ModuleConfigurationPageInterface} from '@interfaces/BO/modules/moduleConfiguration';
 import {type Page} from '@playwright/test';
 
@@ -6,4 +7,5 @@ export interface ModulePsGdprBoMainPageInterface extends ModuleConfigurationPage
   readonly pageSubTitleFr: string;
 
   goToTab(page: Page, nth: number): Promise<boolean>;
+  parseCSVFile(filePath: string | null, encoding: BufferEncoding): Promise<GDPRExportCSV[]>;
 }
