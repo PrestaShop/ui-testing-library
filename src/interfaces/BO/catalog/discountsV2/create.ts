c6ca30683b8e2264eb48174c2aa8a9f93f5df717
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2026-02-26 13:05:45 +0100

    Add interfaces

--- src/interfaces/BO/catalog/discountsV2/create.ts
+++ src/interfaces/BO/catalog/discountsV2/create.ts
@@ -1,7 +1,13 @@
 import type FakerDiscount from '@data/faker/discount';
 import {BOBasePagePageInterface} from '@interfaces/BO';
-import {type Frame, type Page} from '@playwright/test';
+import {type Page} from '@playwright/test';
 
 export interface BODiscountsCreatePageInterface extends BOBasePagePageInterface {
+  readonly pageTitle: string;
+  readonly errorMessageNameRequired: string;
+  readonly errorMessageMinPurchaseAmount: string;
+  readonly errorMessageMinPurchaseAmountNotnumber: string;
+  readonly errorMessageDiscountValue: (discountValue: string) => string;
+
   createDiscount(page: Page, discountData: FakerDiscount): Promise<string>;
 }
--- src/interfaces/BO/catalog/discountsV2/index.ts
+++ src/interfaces/BO/catalog/discountsV2/index.ts
@@ -5,4 +5,7 @@ export interface BODiscountsPageInterface extends BOBasePagePageInterface {
   readonly pageTitle: string;
 
   clickOnCreateDiscountButton(page: Page): Promise<void>;
+  selectDiscountType(page: Page, type: string): Promise<void>;
+  filterDiscount(page: Page, filterType: string, filterBy: string, value: string): Promise<void>;
+  deleteDiscount(page: Page, row?: number): Promise<string>;
 }
