87c5a58276680bc2ebc163005b12d2b32d246d77
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2026-03-11 12:05:40 +0100

    Fix directory name

--- src/pages/BO/catalog/discountsV2/create.ts
+++ src/pages/BO/catalog/discountsV2/create.ts
@@ -7,7 +7,7 @@ const psVersion = testContext.getPSVersion();
 /* eslint-disable global-require, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 function requirePage(): BODiscountsCreatePageInterface {
   if (semver.lt(psVersion, '9.2.0')) {
-    return require('@versions/9.1/pages/BO/catalog/discountV2/create').boDiscountsCreatePage;
+    return require('@versions/9.1/pages/BO/catalog/discountsV2/create').boDiscountsCreatePage;
   }
   return require('@versions/develop/pages/BO/catalog/discountsV2/create').boDiscountsCreatePage;
 }
